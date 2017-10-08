3e3eb09c14037171b499cc6ae1090e2f5a37f21b
Karol Kowalski <ordon1313@gmail.com>
2017-10-08 12:47:52 +0200

    Start with idle state

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginModelTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginModelTest.kt
@@ -9,6 +9,18 @@ class LoginModelTest {
     fun shouldInitLoginModel() {
         Assert.assertNotNull(LoginModel())
     }
+
+    @Test
+    fun shouldStartWithIdleState() {
+        LoginModel().states.test().assertLastValue(Login.State(false))
+    }
 }
 
-class LoginModel
+interface Login {
+    sealed class Event
+    data class State(val loader: Boolean)
+}
+
+class LoginModel : Model<Login.Event, Login.State>(Login.State(false)) {
+    override fun onCleared() = Unit
+}
