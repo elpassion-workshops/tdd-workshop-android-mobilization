a8fcd19544a815de2275bea3cea4409dcda86023
Karol Kowalski <ordon1313@gmail.com>
2017-10-08 12:56:04 +0200

    Call api on login

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginModelTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginModelTest.kt
@@ -1,22 +1,41 @@
 package com.elpassion.mobilization.tddworkshop
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class LoginModelTest {
 
-    private val model = LoginModel()
+    private val api = mock<Login.Api>()
+    private val model = LoginModel(api)
 
     @Test
     fun shouldStartWithIdleState() {
         model.states.test().assertLastValue(Login.State(false))
     }
+
+    @Test
+    fun shouldCallApiOnLogin() {
+        model.events.accept(Login.Event.LoginClicked)
+        verify(api).call()
+    }
 }
 
 interface Login {
-    sealed class Event
+    sealed class Event {
+        object LoginClicked : Event()
+    }
+
     data class State(val loader: Boolean)
+
+    interface Api {
+        fun call()
+    }
 }
 
-class LoginModel : Model<Login.Event, Login.State>(Login.State(false)) {
+class LoginModel(api: Login.Api) : Model<Login.Event, Login.State>(Login.State(false)) {
+    init {
+        api.call()
+    }
     override fun onCleared() = Unit
 }
