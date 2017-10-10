9f584e85daf29b5fc47e57afd37fddec7626f603
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-10-10 18:04:44 +0200

    Fix something?

--- app/src/main/java/com/elpassion/mobilization/tddworkshop/ActionsRelay.kt
+++ app/src/main/java/com/elpassion/mobilization/tddworkshop/ActionsRelay.kt
@@ -1,5 +1,6 @@
 package com.elpassion.mobilization.tddworkshop
 
+import com.jakewharton.rxrelay2.PublishRelay
 import com.jakewharton.rxrelay2.Relay
 import com.jakewharton.rxrelay2.ReplayRelay
 import io.reactivex.Observable
@@ -8,7 +9,7 @@ import io.reactivex.Observer
 class ActionsRelay<Action> : Relay<Action>() {
 
     private var replayRelay = ReplayRelay.create<Action>()
-    private var publishRelay = ReplayRelay.create<Action>()
+    private val publishRelay = PublishRelay.create<Action>()
 
     override fun accept(value: Action) {
         if (publishRelay.hasObservers()) {
