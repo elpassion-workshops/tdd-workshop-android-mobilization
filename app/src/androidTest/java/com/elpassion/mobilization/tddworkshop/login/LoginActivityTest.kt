f8051bb4f1cc7e9e7909f7a39d820d114d442033
Karol Kowalski <ordon1313@gmail.com>
2017-10-17 19:57:55 +0200

    Display password input

--- app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -1,6 +1,9 @@
 package com.elpassion.mobilization.tddworkshop.login
 
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
+import android.text.InputType
 import com.elpassion.android.commons.espresso.*
 import com.elpassion.mobilization.tddworkshop.R
 import org.junit.Rule
@@ -18,7 +21,7 @@ class LoginActivityTest {
 
     @Test
     fun Display_email_input() {
-        onId(R.id.email_input)
+        onId(R.id.emailInput)
                 .isDisplayed()
                 .replaceText("email@wp.pl")
                 .hasText("email@wp.pl")
@@ -27,6 +30,17 @@ class LoginActivityTest {
     @Test
     fun Display_password_header() {
         onText(R.string.password_header).isDisplayed()
+    }
+
+    @Test
+    fun Display_password_input() {
+        onId(R.id.passwordInput)
+                .check(ViewAssertions.matches(
+                        ViewMatchers.withInputType(
+                                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
+                .isDisplayed()
+                .replaceText("password")
+                .hasText("password")
     }
 }
 
