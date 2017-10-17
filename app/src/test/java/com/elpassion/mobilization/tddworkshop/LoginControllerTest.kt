e490a7146d2f17f0cdc2b0b17813eae761a585b8
Karol Kowalski <ordon1313@gmail.com>
2017-10-17 18:05:42 +0200

    Call api on login

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginControllerTest.kt
@@ -2,17 +2,36 @@
 
 package com.elpassion.mobilization.tddworkshop
 
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Assert
 import org.junit.Test
 
 class LoginControllerTest {
 
+    private val api = mock<Login.Api>()
+
     @Test
     fun `Should init login controller`() {
-        Assert.assertNotNull(LoginController())
+        Assert.assertNotNull(LoginController(api))
+    }
+
+    @Test
+    fun `Call api on login`() {
+        LoginController(api).onLogin()
+        verify(api).login()
     }
 }
 
-class LoginController {
+interface Login {
+    interface Api {
+        fun login()
+    }
+}
 
+class LoginController(private val api: Login.Api) {
+
+    fun onLogin() {
+        api.login()
+    }
 }
