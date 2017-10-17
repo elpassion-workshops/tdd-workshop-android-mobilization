46d573920a343cc062e65884e17a717a0712c093
Karol Kowalski <ordon1313@gmail.com>
2017-10-17 20:13:27 +0200

    Call api with provided email and password on login click

--- app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -4,13 +4,27 @@ import android.support.test.rule.ActivityTestRule
 import com.elpassion.android.commons.espresso.*
 import com.elpassion.mobilization.tddworkshop.R
 import com.elpassion.mobilization.tddworkshop.util.hasHiddenInput
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.subjects.SingleSubject
 import org.junit.Rule
 import org.junit.Test
 
 class LoginActivityTest {
 
+    private val loginSubject = SingleSubject.create<Login.User>()
+    private val api = mock<Login.Api>().apply {
+        whenever(login(any(), any())).thenReturn(loginSubject)
+    }
+
     @Rule @JvmField
-    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+    val rule = object : ActivityTestRule<LoginActivity>(LoginActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            LoginActivity.api = api
+        }
+    }
 
     @Test
     fun Display_email_header() {
@@ -37,5 +51,13 @@ class LoginActivityTest {
                 .isDisplayed()
                 .replaceText("password")
                 .hasText("password")
+    }
+
+    @Test
+    fun Call_api_with_provided_email_and_password_on_login_click() {
+        onId(R.id.emailInput).replaceText("email@email.pl")
+        onId(R.id.passwordInput).replaceText("password")
+        onId(R.id.loginButton).click()
+        verify(api).login("email@email.pl", "password")
     }
 }
--- app/src/main/java/com/elpassion/mobilization/tddworkshop/login/LoginActivity.kt
+++ app/src/main/java/com/elpassion/mobilization/tddworkshop/login/LoginActivity.kt
@@ -3,10 +3,18 @@ package com.elpassion.mobilization.tddworkshop.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.elpassion.mobilization.tddworkshop.R
+import kotlinx.android.synthetic.main.login_activity.*
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+        loginButton.setOnClickListener {
+            api.login(emailInput.text.toString(), passwordInput.text.toString())
+        }
+    }
+
+    companion object {
+        lateinit var api: Login.Api
     }
 }
