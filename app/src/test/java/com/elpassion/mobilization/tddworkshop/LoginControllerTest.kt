7e9aa761efc1bb9645714ecaa4ac95a9c52a3aa7
Karol Kowalski <ordon1313@gmail.com>
2017-10-17 18:02:47 +0200

    Init Login Controller

---
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/LoginControllerTest.kt
@@ -0,0 +1,18 @@
+@file:Suppress("IllegalIdentifier")
+
+package com.elpassion.mobilization.tddworkshop
+
+import org.junit.Assert
+import org.junit.Test
+
+class LoginControllerTest {
+
+    @Test
+    fun `Should init login controller`() {
+        Assert.assertNotNull(LoginController())
+    }
+}
+
+class LoginController {
+
+}
