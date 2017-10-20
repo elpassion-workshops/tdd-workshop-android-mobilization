ed49562fcfd666db4406eb494755d0e228422d2a
Nataliia Lapshyna <Nataliia.Lapshyna@seqr.com>
2017-10-20 11:04:25 +0200

    Call api with provided email and password - implemented

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
@@ -2,9 +2,7 @@
 
 package com.elpassion.mobilization.tddworkshop.login
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.never
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
 
 class LoginControllerTest {
@@ -14,19 +12,27 @@ class LoginControllerTest {
     @Test
     fun `Call api on login`() {
         login()
-        verify(api).login()
+        verify(api).login(any(), any())
     }
 
     @Test
     fun `Not call api if email is empty`() {
         login(email = "")
-        verify(api, never()).login()
+        verify(api, never()).login(eq(""), any())
     }
 
     @Test
     fun `Not call api if password is empty`(){
         login(password = "")
-        verify(api, never()).login()
+        verify(api, never()).login("", "password")
+    }
+
+    @Test
+    fun `Call api with provided email and password`() {
+        val email = "email@wp.pl"
+        val password = "password"
+        login(email, password)
+        verify(api).login(email, password)
     }
 
     private fun login(email: String = "email@wp.pl", password: String="password") {
@@ -36,14 +42,14 @@ class LoginControllerTest {
 
 interface Login {
     interface Api {
-        fun login()
+        fun login(login: String, password: String)
     }
 }
 
 class LoginController(private val api: Login.Api) {
     fun login(email: String, password: String) {
         if (email.isNotEmpty() && password.isNotEmpty()) {
-            api.login()
+            api.login(email, password)
         }
     }
 }
