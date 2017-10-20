d97a8a2dae0c499675019521af2085b03901deac
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-10-20 15:18:43 +0200

    LoginActivity init test

---
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -0,0 +1,18 @@
+package com.elpassion.mobilization.tddworkshop.login
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class LoginActivityTest {
+
+    @JvmField
+    @Rule
+    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+
+    @Test
+    fun should_start_activity() {
+
+    }
+}
+
---
+++ app/src/main/java/com/elpassion/mobilization/tddworkshop/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.elpassion.mobilization.tddworkshop.login
+
+import android.app.Activity
+
+class LoginActivity : Activity() {
+
+}
