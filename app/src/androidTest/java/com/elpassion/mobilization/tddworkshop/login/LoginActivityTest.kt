8fdb3c2e425c9ed7a3492880a8c0d2d6d39ddae1
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-10-20 15:35:51 +0200

    Show input email

--- app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -1,8 +1,7 @@
 package com.elpassion.mobilization.tddworkshop.login
 
 import android.support.test.rule.ActivityTestRule
-import com.elpassion.android.commons.espresso.isDisplayed
-import com.elpassion.android.commons.espresso.onText
+import com.elpassion.android.commons.espresso.*
 import com.elpassion.mobilization.tddworkshop.R
 import org.junit.Rule
 import org.junit.Test
@@ -16,6 +15,13 @@ class LoginActivityTest {
     @Test
     fun should_show_email_header() {
         onText(R.string.email_header).isDisplayed()
+    }
+
+    @Test
+    fun should_show_email_input() {
+        onId(R.id.emailInput)
+                .typeText("test@mail.com")
+                .hasText("test@mail.com")
     }
 }
 
