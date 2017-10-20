a0b13ca30f8231baf73c596fb08ab62aba4b6557
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-10-20 10:14:26 +0200

    Not call api if email is empty

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
@@ -3,6 +3,7 @@
 package com.elpassion.mobilization.tddworkshop.login
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
@@ -12,8 +13,15 @@ class LoginControllerTest {
     @Test
     fun `Call api on login`() {
         val api = mock<Login.Api>()
-        LoginController(api).login()
+        LoginController(api).login("emial@wp.pl")
         verify(api, times(1)).login()
+    }
+
+    @Test
+    fun `Not call api if email is empty`() {
+        val api = mock<Login.Api>()
+        LoginController(api).login(email = "")
+        verify(api, never()).login()
     }
 }
 
@@ -24,7 +32,9 @@ interface Login {
 }
 
 class LoginController(private val api: Login.Api) {
-    fun login() {
-        api.login()
+    fun login(email: String) {
+        if (email.isNotEmpty()) {
+            api.login()
+        }
     }
 }
