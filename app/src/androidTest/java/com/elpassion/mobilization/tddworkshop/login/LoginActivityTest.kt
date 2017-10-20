9bd0308b189b85cb8c25c65cd47c4e01d18b5c63
Nataliia Lapshyna <Nataliia.Lapshyna@seqr.com>
2017-10-20 16:54:26 +0200

    Should call api login on login button test

--- app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -3,18 +3,27 @@ package com.elpassion.mobilization.tddworkshop.login
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withInputType
 import android.support.test.rule.ActivityTestRule
+import android.text.InputType
 import android.text.InputType.TYPE_CLASS_TEXT
 import android.text.InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
 import com.elpassion.android.commons.espresso.*
 import com.elpassion.mobilization.tddworkshop.R
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Rule
 import org.junit.Test
 
 class LoginActivityTest {
 
+    private val api = mock<Login.Api>()
+
     @JvmField
     @Rule
-    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+    val rule = object : ActivityTestRule<LoginActivity>(LoginActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            LoginActivity.api = api;
+        }
+    }
 
     @Test
     fun should_show_email_header() {
@@ -38,6 +47,20 @@ class LoginActivityTest {
     @Test
     fun should_show_password_header() {
         shouldShowHeader(R.string.password_header)
+    }
+
+    @Test
+    fun should_show_password_input() {
+        onId(R.id.passwordInput).check(matches(withInputType(TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
+    }
+
+    @Test
+    fun should_call_api_login_on_login_button(){
+        onId(R.id.emailInput).replaceText("mock@email.com")
+        onId(R.id.passwordInput).replaceText("mockPassword")
+        onId(R.id.continueButton).click()
+
+        verify(api).login("mock@email.com","mockPassword")
     }
 
     private fun shouldShowHeader(id: Int) {
