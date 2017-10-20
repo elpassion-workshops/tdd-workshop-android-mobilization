ec645b4c622faa36f9e5c38917ef3535e04c6297
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2017-10-20 16:59:16 +0200

    Should_show_loader_when_login_button_clicked

--- app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/mobilization/tddworkshop/login/LoginActivityTest.kt
@@ -3,10 +3,12 @@ package com.elpassion.mobilization.tddworkshop.login
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withInputType
 import android.support.test.rule.ActivityTestRule
-import android.text.InputType.TYPE_CLASS_TEXT
-import android.text.InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+import android.text.InputType.*
 import com.elpassion.android.commons.espresso.*
 import com.elpassion.mobilization.tddworkshop.R
+import com.elpassion.mobilization.tddworkshop.login.LoginActivity.Companion.api
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,7 +16,11 @@ class LoginActivityTest {
 
     @JvmField
     @Rule
-    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+    val rule = object : ActivityTestRule<LoginActivity>(LoginActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            LoginActivity.api = mock<Login.Api>()
+        }
+    }
 
     @Test
     fun should_show_email_header() {
@@ -42,6 +48,32 @@ class LoginActivityTest {
     @Test
     fun should_show_password_input() {
         onId(R.id.passwordInput).replaceText("password").hasText("password")
+    }
+
+    @Test
+    fun should_password_input_has_type_password() {
+        onId(R.id.passwordInput).check(matches(withInputType(TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD)))
+    }
+
+    @Test
+    fun should_call_login_api_when_login_button_clicked_and_email_and_password_are_provided() {
+        val (email, password) = submitLogin()
+        verify(api).login(email, password)
+    }
+
+    private fun submitLogin(): Pair<String, String> {
+        val email = "test@mail.com"
+        val password = "password"
+        onId(R.id.emailInput).replaceText(email)
+        onId(R.id.passwordInput).replaceText(password)
+        onId(R.id.loginButton).click()
+        return Pair(email, password)
+    }
+
+    @Test
+    fun should_show_loader_when_login_button_clicked() {
+        submitLogin()
+        onText(R.string.loader).isDisplayed()
     }
 }
 
