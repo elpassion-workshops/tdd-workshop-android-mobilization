7e9b969c564b45f03ad1ca0a72eb6f30c484e126
mordowiciel <szymar05@gmail.com>
2017-10-20 12:09:27 +0200

    Do not show next screen until api returns status

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
@@ -3,12 +3,18 @@
 package com.elpassion.mobilization.tddworkshop.login
 
 import com.nhaarman.mockito_kotlin.*
+import io.reactivex.Completable
+import io.reactivex.subjects.CompletableSubject
 import org.junit.Test
 
 class LoginControllerTest {
 
-    private val api = mock<Login.Api>()
+    private val apiSubject = CompletableSubject.create()
+    private val api = mock<Login.Api> {
+        on { login(any(), any()) } doReturn apiSubject
+    }
     private val view = mock<Login.View>()
+
     @Test
     fun `Call api on login`() {
         login()
@@ -24,17 +30,17 @@ class LoginControllerTest {
     @Test
     fun `Not call api if password is empty`() {
         login(password = "")
-        verify(api, never()).login(any(),  any())
+        verify(api, never()).login(any(), any())
     }
 
     @Test
     fun `Call api if email and password are provided`() {
         login()
-        verify(api).login(any(),  any())
+        verify(api).login(any(), any())
     }
 
     @Test
-    fun `Show loader when sending request api`(){
+    fun `Show loader when sending request api`() {
         login()
         verify(view).showLoader()
     }
@@ -42,7 +48,14 @@ class LoginControllerTest {
     @Test
     fun `Show next screen when api request returned success`() {
         login()
+        apiSubject.onComplete()
         verify(view).showAfterLoginScreen()
+    }
+
+    @Test
+    fun `Do not show next screen until api returns status`() {
+        login()
+        verify(view, never()).showAfterLoginScreen()
     }
 
     private fun login(email: String = "email@wp.pl", password: String = "testPassword") {
@@ -52,7 +65,7 @@ class LoginControllerTest {
 
 interface Login {
     interface Api {
-        fun login(email: String, password: String)
+        fun login(email: String, password: String): Completable
     }
 
     interface View {
@@ -61,16 +74,15 @@ interface Login {
     }
 }
 
-class LoginController(private val api: Login.Api, private val view : Login.View) {
+class LoginController(private val api: Login.Api, private val view: Login.View) {
 
-    fun login(email: String, password : String) {
+    fun login(email: String, password: String) {
 
         if (email.isNotEmpty() && password.isNotEmpty()) {
-            api.login(email, password)
+            api.login(email, password).subscribe {
+                view.showAfterLoginScreen()
+            }
             view.showLoader()
-            view.showAfterLoginScreen()
         }
-
-
     }
 }
