ec6ed00152ca1093758ac18e97d436de42a4c5d3
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-10-20 09:54:22 +0200

    Init LoginController

---
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
@@ -0,0 +1,16 @@
+@file:Suppress("IllegalIdentifier")
+
+package com.elpassion.mobilization.tddworkshop.login
+
+import org.junit.Test
+
+class LoginControllerTest {
+
+    @Test
+    fun `Init test`() {
+        LoginController()
+    }
+
+}
+
+class LoginController
