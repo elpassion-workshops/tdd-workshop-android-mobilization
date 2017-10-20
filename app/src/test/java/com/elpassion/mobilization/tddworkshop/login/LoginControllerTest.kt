2360025ae1effb182fa99ca0c943b60d29cc8713
mordowiciel <szymar05@gmail.com>
2017-10-20 11:11:49 +0200

    [Refactoring] Refactored test to use any() instead of hardcoded values

--- app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
+++ app/src/test/java/com/elpassion/mobilization/tddworkshop/login/LoginControllerTest.kt
@@ -2,9 +2,7 @@
 
 package com.elpassion.mobilization.tddworkshop.login
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.never
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Test
 
 class LoginControllerTest {
@@ -14,25 +12,25 @@ class LoginControllerTest {
     @Test
     fun `Call api on login`() {
         login()
-        verify(api).login("testEmail@wp.pl", "testPassword")
+        verify(api).login(any(), any())
     }
 
     @Test
     fun `Not call api if email is empty`() {
         login(email = "")
-        verify(api, never()).login("testEmail@wp.pl", "testPassword")
+        verify(api, never()).login(any(), any())
     }
 
     @Test
     fun `Not call api if password is empty`() {
         login(password = "")
-        verify(api, never()).login("testEmail@wp.pl", "testPassword")
+        verify(api, never()).login(any(),  any())
     }
 
     @Test
     fun `Call api if email and password are provided`() {
         login()
-        verify(api).login("testEmail@wp.pl", "testPassword")
+        verify(api).login(any(),  any())
     }
 
     private fun login(email: String = "email@wp.pl", password: String = "testPassword") {
@@ -47,9 +45,10 @@ interface Login {
 }
 
 class LoginController(private val api: Login.Api) {
+
     fun login(email: String, password : String) {
         if (email.isNotEmpty() && password.isNotEmpty()) {
-            api.login("testEmail@wp.pl", "testPassword")
+            api.login(email, password)
         }
     }
 }
